a34b2b4c8c7d29c61118b9a840f428cbeeb096b1
nathan tse <xiezhuogang@gmail.com>
2022-03-29 13:30:31 +0800

    使用新的密码管理模型

--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -923,7 +923,7 @@ class WechatGamePasswd(models.Model):
     app = models.ForeignKey(WechatApp, default=None, on_delete=models.CASCADE)
     password = models.CharField(max_length=50, primary_key=True)
     assigned_player = models.ForeignKey(WechatPlayer, default=None, on_delete=models.CASCADE, blank=True)
-    is_assigned = models.BooleanField(default=False, verbose='是否已分配')
+    is_assigned = models.BooleanField(default=False, verbose_name='是否已分配')
 
     def __str__(self):
         return self.password
