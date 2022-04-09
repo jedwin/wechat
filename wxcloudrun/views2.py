52036f9011d33038ea73d35c3687b8e43466e44a
jedwin <xiezhuogang@gmail.com>
2022-04-09 12:23:51 +0800

    更新链接中指定游戏名称功能

--- wxcloudrun/views2.py
+++ wxcloudrun/views2.py
@@ -558,9 +558,8 @@ def get_user_info_with_code(request):
     except ObjectDoesNotExist:
         # app_en_name参数传递错误
         errmsg = f'app_en_name参数传递错误: {app_en_name}，或者没有找到对应的用户{openid}'
-
-    if game_name == 'game1':
-        cur_game_name = '老广新痕（4月版）'
+    if len(game_name) > 0:
+        cur_game_name = game_name
     redirect_url = f'/profile/?app_en_name={app_en_name}&cur_game_name={cur_game_name}&openid={openid}&errmsg={errmsg}#wechat_redirect'
     return HttpResponseRedirect(redirect_url)
 
