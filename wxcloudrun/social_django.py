35440895f204c9e52eab2935d2a7f6e1a66428fc
nathan tse <xiezhuogang@gmail.com>
2023-05-25 16:47:59 +0800

    remove OAuth2 login buttons in login.html

--- wxcloudrun/social_django.py
+++ wxcloudrun/social_django.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import *
-import google_auth_oauthlib.flow
-from googleapiclient.discovery import build
+# import google_auth_oauthlib.flow
+# from googleapiclient.discovery import build
 import os
 from django.shortcuts import HttpResponse, render, HttpResponseRedirect
 from django.http import JsonResponse
@@ -79,76 +79,75 @@ CLIENT_SECRETS_FILE = "client_secret.json"
 API_SERVICE_NAME = 'oauth2'
 API_VERSION = 'v2'
 
-def authorize_google(request):
-    # 先保存要返回的URL
-    next_url = request.GET.get('next', DEFAULT_NEXT_URL)
-    
-    # 先判断当前用户是否已经登录，如果已经登录，则直接跳转到首页
-    if request.user.is_authenticated:
-        return HttpResponseRedirect(next_url)
-    else:
-        request.session['next_url'] = next_url
-        # 开始进行Google OAuth2.0认证
-        # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
-        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-                CLIENT_SECRETS_FILE, scopes=SCOPES)
-
-        # The URI created here must exactly match one of the authorized redirect URIs
-        # for the OAuth 2.0 client, which you configured in the API Console. If this
-        # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
-        # error.
-        # flow.redirect_uri = 'https://www.key2go.top:8443/code'
-        flow.redirect_uri = os.environ.get("GOOGLE_API_REDIRECT_URI")
-
-        authorization_url, state = flow.authorization_url(
-                # Enable offline access so that you can refresh an access token without
-                # re-prompting the user for permission. Recommended for web server apps.
-                access_type='offline',
-                # Enable incremental authorization. Recommended as a best practice.
-                include_granted_scopes='true')
-        # 将state放入当前会话中
-        request.session['state'] = state
-
-        logger.info(f'state={state}')
-        # logger.info(f'authorization_url={authorization_url}')
-        return HttpResponseRedirect(authorization_url)
-
-
-def callback_google(request):
-    # Specify the state when creating the flow in the callback so that it can
-    # verify in the authorization server response.
-    state = request.GET.get('state', '')
-    if state != request.session.get('state', ''):
-        # The state in the response doesn't match the state in the request.
-        # 可能是CSRF攻击
-        return HttpResponse('The state is not correct, please try again.')
-    else:
-        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-                CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
-        flow.redirect_uri = os.environ.get("GOOGLE_API_REDIRECT_URI")
-
-        # Use the authorization server's response to fetch the OAuth 2.0 tokens.
-        authorization_response = request.build_absolute_uri()
-        flow.fetch_token(authorization_response=authorization_response)
-
-        # Store credentials in the session.
-        # ACTION ITEM: In a production app, you likely want to save these
-        #                            credentials in a persistent database instead.
-        credentials = flow.credentials
-        credentials_dict = credentials_to_dict(credentials)
-        # token = credentials_dict['token']
-        logger.info(credentials_dict)
-        user_profile = build('oauth2', 'v2', credentials=credentials)
-        google_user_info = user_profile.userinfo().get().execute()
-        google_id = google_user_info['id']
-        user = find_user(id=google_id, auth_user_info=google_user_info, auth_type='google')
-        if user:
-            # 取回要返回的URL
-            next_url = request.session.get('next_url', DEFAULT_NEXT_URL)
-            login(request, user)
-            return HttpResponseRedirect(next_url)
-        else:
-            return HttpResponse(f'无法找到对应的玩家。google_user_info: {google_user_info}')
+# def authorize_google(request):
+#     # 先保存要返回的URL
+#     next_url = request.GET.get('next', DEFAULT_NEXT_URL)
+#
+#     # 先判断当前用户是否已经登录，如果已经登录，则直接跳转到首页
+#     if request.user.is_authenticated:
+#         return HttpResponseRedirect(next_url)
+#     else:
+#         request.session['next_url'] = next_url
+#         # 开始进行Google OAuth2.0认证
+#         # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+#         flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+#                 CLIENT_SECRETS_FILE, scopes=SCOPES)
+#
+#         # The URI created here must exactly match one of the authorized redirect URIs
+#         # for the OAuth 2.0 client, which you configured in the API Console. If this
+#         # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
+#         # error.
+#         # flow.redirect_uri = 'https://www.key2go.top:8443/code'
+#         flow.redirect_uri = os.environ.get("GOOGLE_API_REDIRECT_URI")
+#
+#         authorization_url, state = flow.authorization_url(
+#                 # Enable offline access so that you can refresh an access token without
+#                 # re-prompting the user for permission. Recommended for web server apps.
+#                 access_type='offline',
+#                 # Enable incremental authorization. Recommended as a best practice.
+#                 include_granted_scopes='true')
+#         # 将state放入当前会话中
+#         request.session['state'] = state
+#
+#         logger.info(f'state={state}')
+#         # logger.info(f'authorization_url={authorization_url}')
+#         return HttpResponseRedirect(authorization_url)
+#
+# def callback_google(request):
+#     # Specify the state when creating the flow in the callback so that it can
+#     # verify in the authorization server response.
+#     state = request.GET.get('state', '')
+#     if state != request.session.get('state', ''):
+#         # The state in the response doesn't match the state in the request.
+#         # 可能是CSRF攻击
+#         return HttpResponse('The state is not correct, please try again.')
+#     else:
+#         flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+#                 CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+#         flow.redirect_uri = os.environ.get("GOOGLE_API_REDIRECT_URI")
+#
+#         # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+#         authorization_response = request.build_absolute_uri()
+#         flow.fetch_token(authorization_response=authorization_response)
+#
+#         # Store credentials in the session.
+#         # ACTION ITEM: In a production app, you likely want to save these
+#         #                            credentials in a persistent database instead.
+#         credentials = flow.credentials
+#         credentials_dict = credentials_to_dict(credentials)
+#         # token = credentials_dict['token']
+#         logger.info(credentials_dict)
+#         user_profile = build('oauth2', 'v2', credentials=credentials)
+#         google_user_info = user_profile.userinfo().get().execute()
+#         google_id = google_user_info['id']
+#         user = find_user(id=google_id, auth_user_info=google_user_info, auth_type='google')
+#         if user:
+#             # 取回要返回的URL
+#             next_url = request.session.get('next_url', DEFAULT_NEXT_URL)
+#             login(request, user)
+#             return HttpResponseRedirect(next_url)
+#         else:
+#             return HttpResponse(f'无法找到对应的玩家。google_user_info: {google_user_info}')
 
 
 def credentials_to_dict(credentials):
