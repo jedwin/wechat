f09c94c6578ca6ac678cd359b3c3a65b6636e9cc
jedwin <xiezhuogang@gmail.com>
2023-11-08 23:25:50 +0800

    react js succ

--- wxcloudrun/static/js/react.js
+++ wxcloudrun/static/js/react.js
@@ -1,13 +1,31 @@
-import { createRoot } from 'react-dom/client';
-console.log("react.js loaded");
-function HintButton() {
+//import { createRoot } from 'react-dom/client';
+let timeout = 500;
+const showHint = () => {
+    // show the confirm window, if user click the confirm button, then show the hint string
+    
+    weui.confirm('<h1>确定吗？</h1>', 
+            function () {
+                setTimeout(function () {
+                    weui.alert('好的');
+                }, timeout);
+            }, function () {
+                setTimeout(function() {
+                    weui.alert('不好');
+                }, timeout);
+            }
+    );
+}
+
+const HintButton = () => {
     return(
             <div>
-                <a role="button" class="weui-btn weui-btn_default" >tip</a>
+                <a role="button" className="weui-btn weui-btn_default" onClick={showHint} >tip</a>
             </div>
         );
 }
+// create a function to show the hint string in popup window
 
-const domNode = document.getElementById('hint');
-const root = createRoot(domNode);
-root.render(<HintButton />);
+ReactDOM.render(
+    <HintButton />,
+    document.querySelector('#hint')
+);
