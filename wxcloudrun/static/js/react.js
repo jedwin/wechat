abca082e77a4d59ed2715eff30736d52c3fdbba6
jedwin <xiezhuogang@gmail.com>
2023-11-09 23:44:40 +0800

    react succ 3

--- wxcloudrun/static/js/react.js
+++ wxcloudrun/static/js/react.js
@@ -1,3 +1,10 @@
-//import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 
-
+function CommonDialog({title, content}) {
+    
+    return(
+        <div>
+            <a role="button" className="weui-btn weui-btn_default" onClick={()=>{weui.alert({content});}}>{title}</a>
+        </div>
+    );
+}
