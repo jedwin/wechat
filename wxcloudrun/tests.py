8370c0ecdb062d7408c9de04e4b828901ae39feb
jedwin <xiezhuogang@gmail.com>
2024-03-28 09:56:44 +0800

    0.47

--- wxcloudrun/tests.py
+++ wxcloudrun/tests.py
@@ -23,6 +23,18 @@ TEST_MAX_STEP = 40  # test_process中最大测试步数
 SHOW_PROCESS = False  # 是否显示测试关卡过程
 RANDOM_ANSWER_STRING = 'aghiqrsyz01289' # 随机答案字符串
 
+def print_green(content):
+    print_green(f'\033[32m{content}\033[0m')
+
+def print_red(content):
+    print_red(f'\033[31m{content}\033[0m')
+
+def print_blue(content):
+    print_blue(f'\033[34m{content}\033[0m')
+
+def print_yellow(content):
+    print(f'\033[33m{content}\033[0m')
+
 class TestModel(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -51,28 +63,28 @@ class TestModel(TestCase):
         self.factory = RequestFactory()
 
     def test_login(self):
-        print(f'开始测试\033[34m【系统权限逻辑】\033[0m')
+        print(f'开始测试\033[34m【系统权限逻辑】')
         request = self.factory.get(f"/game/?app_en_name={TEST_APP_NAME}&game_name={self.game.name}")
 
         # 测试未登录时的返回
         request.user = AnonymousUser()
         response = views2.game(request)
         self.assertEqual(type(response), HttpResponseRedirect)
-        print('\033[32m用户未登录时，重定向登录页面正常\033[0m')
+        print('\033[32m用户未登录时，重定向登录页面正常')
 
         # 测试无权限时的返回
         request.user = self.user
         response = views2.game(request)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, f"无权限进入本游戏")
-        print('\033[32m对无权限玩家，页面内容显示正常\033[0m')
+        print('\033[32m对无权限玩家，页面内容显示正常')
 
         # 测试有权限时的返回
         # group = Group.objects.get(name=TEST_GAME_NAME)
         self.user.groups.add(self.group)
         response = views2.game(request)
         self.assertContains(response, self.game.show_opening())
-        print('\033[32m对有权限玩家，游戏开场画面显示正常\033[0m')
+        print('\033[32m对有权限玩家，游戏开场画面显示正常')
 
         # 游戏未激活时的返回
         self.game.is_active = False
@@ -80,7 +92,7 @@ class TestModel(TestCase):
         response = views2.game(request)
         # print(response.content.decode('utf-8'))
         self.assertContains(response, views2.GAME_IS_NOT_ACTIVE)
-        print('\033[32m对已分配权限，但游戏未激活时，提示内容显示正常\033[0m')
+        print('\033[32m对已分配权限，但游戏未激活时，提示内容显示正常')
         self.game.is_active = True
         self.game.save()
         
@@ -90,14 +102,14 @@ class TestModel(TestCase):
         response = views2.game(request)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, f"请选择你要进行的游戏")
-        print('\033[32m对有权限玩家不指定游戏名时，游戏选择画面返回正常\033[0m')
+        print('\033[32m对有权限玩家不指定游戏名时，游戏选择画面返回正常')
 
         # 设置为直接进入游戏时的返回
         self.app.auto_enter_game = True
         self.app.save()
         response = views2.game(request)
         self.assertContains(response, self.game.show_opening())
-        print('\033[32m对有权限玩家不指定游戏，但打开直接进入游戏开关时，游戏开场内容显示正常\033[0m')
+        print('\033[32m对有权限玩家不指定游戏，但打开直接进入游戏开关时，游戏开场内容显示正常')
         
     def test_process(self):
         print(f'开始测试游戏\033[34m【{self.game.name}】\033[0m流程')
@@ -121,9 +133,9 @@ class TestModel(TestCase):
         else:
             raise ValueError(f'Unsupported file format: {TEST_SETTINGS_FILE}')
         if result_dict['result']:
-            print(f'\033[32m{result_dict["errmsg"]}\033[0m')
+            print_green(f'{result_dict["errmsg"]}')
         else:
-            print(f'\033[31m{result_dict["errmsg"]}\033[0m')
+            print_red(f'{result_dict["errmsg"]}')
 
         # 开始测试游戏流程
         entry_quest = ExploreGameQuest.objects.get(game=self.game, quest_trigger=self.game.entry)
@@ -138,7 +150,7 @@ class TestModel(TestCase):
         response = views2.game(request)
         # print(response.content.decode('utf-8'))
         self.assertContains(response, self.game.show_opening())
-        print(f'\033[32m【游戏开场】\033[0m内容显示正常')
+        print_green(f'【游戏开场】\033[0m内容显示正常')
 
         # 初始化玩家状态
         reward_list = list()
@@ -194,16 +206,16 @@ class TestModel(TestCase):
                         self.assertNotContains(response, f"option_click('{item}');")
                         not_available_answers_list.append(item)
                 if len(available_answers_list) == 0:
-                    if SHOW_PROCESS: print(f'\033[31m【注意】\033[0m：关卡\033[34m【{cur_quest.quest_trigger}】\033[0m是\033[32m选择题\033[0m，但没有可用选项，无法继续测试。可能是前置条件设置有误或关卡缺失')
+                    if SHOW_PROCESS: print_red(f'【注意】\033[0m：关卡\033[34m【{cur_quest.quest_trigger}】\033[0m是\033[32m选择题\033[0m，但没有可用选项，无法继续测试。可能是前置条件设置有误或关卡缺失')
                     break
                 # 至少有一个可用选项
                 if SHOW_PROCESS: print(f'关卡\033[34m【{cur_quest.quest_trigger}】\033[0m是\033[32m选择题\033[0m，{num_of_next}个可选项都显示正常：{available_answers_list}')
                 option_count += 1
                 if len(not_available_answers_list) > 0:
-                    if SHOW_PROCESS: print(f'\033[31m【注意】\033[0m：本关卡关联了{len(not_available_answers_list)}个缺失关卡 {not_available_answers_list}')
+                    if SHOW_PROCESS: print_red(f'【注意】\033[0m：本关卡关联了{len(not_available_answers_list)}个缺失关卡 {not_available_answers_list}')
                 # 随机选择一个选项作为答案
                 answer = available_answers_list[randint(0, num_of_next-1)]
-                if SHOW_PROCESS: print(f'随机选择的答案是：\033[43m【{answer}】\033[0m')
+                if SHOW_PROCESS: print(f'随机选择的答案是：\033[43m【{answer}】')
                 next_quest_trigger = answer
             else:
                 # 检查选择题的答案框显示
@@ -214,24 +226,24 @@ class TestModel(TestCase):
                 num_of_answer = len(answer_list)
                 # 随机选择一个作为答案
                 answer = answer_list[randint(0, num_of_answer-1)]
-                if SHOW_PROCESS: print(f'随机选择的答案是：\033[43m【{answer}】\033[0m')
+                if SHOW_PROCESS: print(f'随机选择的答案是：\033[43m【{answer}】')
                 next_quest_trigger = cur_quest.next_list.replace('｜', '|').split('|')[0]
             try:
                 next_quest = ExploreGameQuest.objects.get(game=self.game, quest_trigger=next_quest_trigger)
             except ExploreGameQuest.DoesNotExist:
-                print(f'\033[31m【注意】\033[0m：下一个关卡\033[31m【{next_quest_trigger}】\033[0m不存在，无法继续测试')
+                print_red(f'【注意】\033[0m：下一个关卡\033[31m【{next_quest_trigger}】\033[0m不存在，无法继续测试')
                 break
             # 更新奖励列表
             if cur_quest.reward_id > 0 and cur_quest.reward_id not in reward_list:
                 reward_list.append(cur_quest.reward_id)
-                if SHOW_PROCESS: print(f'正常进入关卡\033[34m【{cur_quest.quest_trigger}】\033[0m，并获得奖励ID：\033[45m【{cur_quest.reward_id}】\033[0m')
+                if SHOW_PROCESS: print(f'正常进入关卡\033[34m【{cur_quest.quest_trigger}】\033[0m，并获得奖励ID：\033[45m【{cur_quest.reward_id}】')
                 # 判断是否已满足通关条件，如果是就跳出循环
                 if set(clear_requirement_list).issubset(set(reward_list)):
                     is_cleared = True
                     # print(f'已满足通关条件：{clear_requirement_list}')
                     break
             else:
-                if SHOW_PROCESS: print(f'正常进入关卡\033[34m【{cur_quest.quest_trigger}】\033[0m')
+                if SHOW_PROCESS: print(f'正常进入关卡\033[34m【{cur_quest.quest_trigger}】')
             
             # 检查玩家等待状态是否正确
             self.player = WechatPlayer.objects.get(name=TEST_USER_NAME)
@@ -242,9 +254,9 @@ class TestModel(TestCase):
             # 准备进入下一个关卡
             cur_quest = next_quest
             if cur_quest.next_list == '':
-                print(f'\033[31m【注意】\033[0m：关卡\033[31m【{cur_quest.quest_trigger}】\033[0m的\033[31m下一步为空\033[0m，无法继续测试')
+                print_red(f'【注意】\033[0m：关卡\033[31m【{cur_quest.quest_trigger}】\033[0m的\033[31m下一步为空\033[0m，无法继续测试')
                 break
-            if SHOW_PROCESS: print(f'目前获得奖励列表：\033[35m{reward_list}\033[0m')
+            if SHOW_PROCESS: print(f'目前获得奖励列表：\033[35m{reward_list}')
 
             # 在mod 4 = 2时测试回答错误时的返回
             if num_of_step % 4 == 2:
@@ -252,7 +264,7 @@ class TestModel(TestCase):
                 request.user = self.user
                 response = views2.game(request)
                 self.assertContains(response, 'id="errorTips"')
-                if SHOW_PROCESS: print(f'\033[34m第{num_of_step}步回答错误时，错误提示显示正常\033[0m')
+                if SHOW_PROCESS: print_blue(f'第{num_of_step}步回答错误时，错误提示显示正常')
                 error_tips_count += 1
 
 
@@ -264,7 +276,7 @@ class TestModel(TestCase):
             response = views2.game(request)
             self.assertContains(response, self.game.show_clear_notice())
             self.assertContains(response, 'id="showClearCode"')
-            print(f'\033[32m第一次通关时，内容显示正常\033[0m')
+            print_green(f'第一次通关时，内容显示正常')
 
             # 再检查一次进入游戏，是否正确显示通关内容和通关码按钮
             answer = 'jfdaealrtflg'  # 随便一个答案
@@ -273,25 +285,25 @@ class TestModel(TestCase):
             response = views2.game(request)
             self.assertContains(response, self.game.show_clear_notice())
             self.assertContains(response, 'id="showClearCode"')
-            print(f'\033[32m已通关后再次进入游戏时，通关画面显示正常\033[0m')
+            print_green(f'已通关后再次进入游戏时，通关画面显示正常')
             test_result = True
         
         # 判断是否完成了测试步骤
-        if content_is_ok: print(f'\033[32m{content_count}次进入关卡，内容显示正常\033[0m')
-        if hint_is_ok: print(f'\033[32m{hint_count}个关卡中配置有提示，内容显示正常\033[0m')
-        if option_is_ok: print(f'\033[32m{option_count}条选择题，选项显示正常\033[0m')
-        if answer_is_ok: print(f'\033[32m{answer_count}条问答题，答题框显示正常\033[0m')
-        if error_tips_is_ok: print(f'\033[32m{error_tips_count}次故意回答错误时，错误提示显示正常\033[0m')
+        if content_is_ok: print_green(f'{content_count}次进入关卡，内容显示正常')
+        if hint_is_ok: print_green(f'{hint_count}个关卡中配置有提示，内容显示正常')
+        if option_is_ok: print_green(f'{option_count}条选择题，选项显示正常')
+        if answer_is_ok: print_green(f'{answer_count}条问答题，答题框显示正常')
+        if error_tips_is_ok: print_green(f'{error_tips_count}次故意回答错误时，错误提示显示正常')
         if len(reward_list) > 0: 
-            print(f'\033[32m获得奖励ID：{reward_list}\033[0m') 
+            print_green(f'获得奖励ID：{reward_list}') 
         else: 
-            print(f'\033[32m未获得任何奖励\033[0m')
+            print_green(f'未获得任何奖励')
         if test_result:
-            print(f'\033[32m流程测试完成，顺利达到通关条件\033[0m')
+            print_green(f'流程测试完成，顺利达到通关条件')
         elif num_of_step >= TEST_MAX_STEP:
-            print(f'\033[31m流程测试未完成\033[0m, 已达到最大测试步数{TEST_MAX_STEP}仍未能通关')
+            print_red(f'流程测试未完成\033[0m, 已达到最大测试步数{TEST_MAX_STEP}仍未能通关')
         else:
-            print(f'\033[31m流程测试未完成\033[0m, 共进行{num_of_step}步')
+            print_red(f'流程测试未完成\033[0m, 共进行{num_of_step}步')
 
     def test_ExplorerGame_and_Quest(self):
         # 测试ExploreGame类
@@ -310,38 +322,73 @@ class TestModel(TestCase):
         ret_dict = handle_player_command(game_name=self.game.name, 
                                          open_id=self.user.id, cmd='test', user_name=self.user.username)
         self.assertEqual(ret_dict['error_msg'], 'app_en_name is blank')
-        print(f'\033[32m不带app名调用，返回正常\033[0m')
+        print_green(f'不带app名调用，返回正常')
 
         # 乱写app名调用
         wrong_app = 'wrong_app'
         ret_dict = handle_player_command(app_en_name=wrong_app, game_name=self.game.name, 
                                          open_id=self.user.id, cmd='test', user_name=self.user.username)
         self.assertEqual(ret_dict['error_msg'], f'app_en_name:{wrong_app} 不存在')
-        print(f'\033[32m乱写app名调用，返回正常\033[0m')
+        print_green(f'乱写app名调用，返回正常')
 
         # 不带游戏名调用
         ret_dict = handle_player_command(app_en_name=self.app.en_name, 
                                          open_id=self.user.id, cmd='test', user_name=self.user.username)
         self.assertEqual(ret_dict['error_msg'], 'game_name is blank')
-        print(f'\033[32m不带游戏名调用，返回正常\033[0m')
+        print_green(f'不带游戏名调用，返回正常')
 
         # 乱写游戏名调用
         wrong_game = 'wrong_game'
         ret_dict = handle_player_command(app_en_name=self.app.en_name, game_name=wrong_game, 
                                          open_id=self.user.id, cmd='test', user_name=self.user.username)
         self.assertEqual(ret_dict['error_msg'], f'游戏{wrong_game}不存在')
-        print(f'\033[32m乱写游戏名调用，返回正常\033[0m')
+        print_green(f'乱写游戏名调用，返回正常')
 
         # 不带用户名调用
         ret_dict = handle_player_command(app_en_name=self.app.en_name, game_name=self.game.name, 
                                          open_id=self.user.id, cmd='test')
         self.assertEqual(ret_dict['error_msg'], 'user_name is blank')
-        print(f'\033[32m不带用户名调用，返回正常\033[0m')
+        print_green(f'不带用户名调用，返回正常')
+
+        # 游戏未启用时调用
+        self.game.is_active = False
+        self.game.save()
+        ret_dict = handle_player_command(app_en_name=self.app.en_name, game_name=self.game.name, 
+                                         open_id=self.user.id, cmd='test', user_name=self.user.username)
+        self.assertEqual(ret_dict['error_msg'], f'游戏{self.game.name}未启动或已过活动时间')
+        print_green(f'游戏未启用时调用，返回正常')
+        self.game.is_active = True
+        self.game.save()
+
+        # 游戏没有配置入口任务
+        self.game.entry = ''
+        self.game.save()
+        ret_dict = handle_player_command(app_en_name=self.app.en_name, game_name=self.game.name, 
+                                         open_id=self.user.id, cmd='test', user_name=self.user.username)
+        self.assertEqual(ret_dict['error_msg'], f'游戏{self.game.name}没有入口任务')
+        print_green(f'游戏没有配置入口时调用，返回正常')
+        self.game.entry = TEST_GAME_ENTRY
+        self.game.save()
+
+        # 游戏入口任务不存在
+        self.game.entry = 'wrong_entry'
+        self.game.save()
+        ret_dict = handle_player_command(app_en_name=self.app.en_name, game_name=self.game.name, 
+                                         open_id=self.user.id, cmd='test', user_name=self.user.username)
+        self.assertEqual(ret_dict['error_msg'], f'游戏{self.game.name}入口任务{self.game.entry}不存在')
+        print_green(f'游戏入口任务不存在时调用，返回正常')
+
+        # 游戏入口任务不存在，且cmd为空，且wait_status为空，应返回所有可进入的关卡
+        ret_dict = handle_player_command(app_en_name=self.app.en_name, game_name=self.game.name, 
+                                         open_id=self.user.id, user_name=self.user.username)
+        self.assertEqual(len(ret_dict['reply_options']), 3)
+        print_green(f'游戏入口任务不存在，且cmd为空，且wait_status为空时调用，正常返回所有可进入关卡')
+        self.game.entry = TEST_GAME_ENTRY
+        self.game.save()
 
         # 乱写用户名调用，可以正常使用
         wrong_user = 'wrong_user'
         ret_dict = handle_player_command(app_en_name=self.app.en_name, game_name=self.game.name, 
                                          open_id=self.user.id, cmd='test', user_name=wrong_user)
         self.assertEqual(ret_dict['error_msg'], '')
-        print(f'\033[32m乱写用户名调用，返回正常\033[0m')
-
+        print_green(f'乱写用户名调用，返回正常')
